ef16e7f15b60bbba43d772fc7fb830fb2fb8a3e4
Luis <luisfernandosantaospina@gmail.com>
2023-03-01 17:21:46 -0500

    Add router outlet

--- src/app/components/home/home-routing.module.ts
+++ src/app/components/home/home-routing.module.ts
@@ -1,9 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home.component";
+import { UserComponent } from "./user/user.component";
+import { ClientComponent } from "./client/client.component";
+import { ProductComponent } from "./product/product.component";
+import { RoleComponent } from "./role/role.component";
+import { OrderComponent } from "./order/order.component";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: 'users', component: UserComponent},
+      { path: 'rols', component: RoleComponent},
+      { path: 'clients', component: ClientComponent},
+      { path: 'products', component: ProductComponent},
+      { path: 'orders', component: OrderComponent},
+      // { path: 'ordenes/crear', component: CreateOrderComponent},
+      // { path: 'ordenes/editar/:id', component: EditOrderComponent},
+      // { path: 'productos/crear', component: CreateProductComponent},
+      // { path: 'productos/editar/:id', component: EditProductComponent},
+      // { path: 'usuarios/crear', component: CreateUserComponent},
+      // { path: 'usuarios/editar/:id', component: EditUserComponent},
+      // { path: 'clientes/crear', component: CreateClientComponent},
+      // { path: 'clientes/editar/:id', component: EditClientComponent},
+      // { path: 'roles/crear', component: CreateRolComponent},
+      // { path: 'roles/editar/:id', component: EditRoleComponent}
+    ]}
 ];
 
 @NgModule({
